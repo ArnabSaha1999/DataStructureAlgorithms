42976630493de1d9e74910e0e78c87d946ab61c5
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2022-02-26 23:08:37 +0530

    Add files via upload

---
+++ StarPatterns/HalfStarDiamondPattern.java
@@ -0,0 +1,21 @@
+package StarPattern;
+
+import java.util.Scanner;
+
+public class HalfStarDiamondPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of rows : ");
+        int row = sc.nextInt();
+        sc.close();
+        for (int i = 0; i < row; i++) {
+            for (int sp = 1; sp < row - i; sp++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
