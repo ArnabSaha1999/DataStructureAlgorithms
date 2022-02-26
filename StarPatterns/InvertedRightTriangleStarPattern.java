21a6c114574f43119d12f43384fbe6b63477d9e2
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2022-02-26 19:39:38 +0530

    Add files via upload

---
+++ StarPatterns/InvertedRightTriangleStarPattern.java
@@ -0,0 +1,18 @@
+package StarPattern;
+
+import java.util.Scanner;
+
+public class InvertedRightTriangleStarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of rows : ");
+        int row = sc.nextInt();
+        sc.close();
+        for (int i = 0; i < row; i++) {
+            for (int j = row - i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
