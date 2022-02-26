6812b7b8cc7ccf255f3ed4de2975550f0255d352
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2022-02-26 19:25:16 +0530

    Create RightTriangleStarPattern.java

---
+++ StarPatterns/RightTriangleStarPattern.java
@@ -0,0 +1,18 @@
+package StarPattern;
+
+import java.util.Scanner;
+
+class RightTriangleStarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of rows : ");
+        int row = sc.nextInt();
+        sc.close();
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
