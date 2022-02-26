fcd0917910400876a17fe0e31bb1018ac7711460
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2022-02-26 19:35:57 +0530

    Add files via upload

---
+++ StarPatterns/LeftTriangleStarPattern.java
@@ -0,0 +1,21 @@
+package StarPattern;
+
+import java.util.Scanner;
+
+class LeftTriangleStarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of rows : ");
+        int row = sc.nextInt();
+        sc.close();
+        for (int i = 0; i < row; i++) {
+            for (int sp = 0; sp < row - i - 1; sp++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
