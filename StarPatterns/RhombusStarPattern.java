a46fed7f4245bbe8311161952d30f7235ff6d6f8
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2022-02-26 20:16:54 +0530

    Add files via upload

---
+++ StarPatterns/RhombusStarPattern.java
@@ -0,0 +1,23 @@
+package StarPattern;
+
+import java.util.Scanner;
+
+public class RhombusStarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of rows : ");
+        int row = sc.nextInt();
+        System.out.print("Enter the Number of cols : ");
+        int col = sc.nextInt();
+        sc.close();
+        for (int i = 0; i < row; i++) {
+            for (int sp = 0; sp < row - i; sp++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < col; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
