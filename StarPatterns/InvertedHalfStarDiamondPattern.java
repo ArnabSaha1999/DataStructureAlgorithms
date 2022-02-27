7f2193a441397202f4885c007c75c836faacb214
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2022-02-27 20:57:29 +0530

    Add files via upload

---
+++ StarPatterns/InvertedHalfStarDiamondPattern.java
@@ -0,0 +1,24 @@
+package StarPattern;
+
+import java.util.Scanner;
+
+public class InvertedHalfStarDiamondPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of rows : ");
+        int row = sc.nextInt();
+        sc.close();
+        for (int i = 0; i < row; i++) {
+            for (int sp = 1; sp <= i; sp++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < row - i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 1; k < row - i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
