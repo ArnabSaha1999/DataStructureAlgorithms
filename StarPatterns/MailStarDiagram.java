9441b499c53fd6228f8f4027f438fe6e054cc4f8
ARNAB SAHA <83657302+ArnabSaha1999@users.noreply.github.com>
2022-02-28 21:32:33 +0530

    Add files via upload

---
+++ StarPatterns/MailStarDiagram.java
@@ -0,0 +1,69 @@
+package StarPattern;
+
+import java.util.Scanner;
+
+public class MailStarDiagram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number of rows : ");
+        int row = sc.nextInt();
+        sc.close();
+        for (int i = 0; i < row; i++) {
+            for (int sp = 1; sp < row - i; sp++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int sp = 1; sp < row - i; sp++) {
+                System.out.print(" ");
+            }
+            for (int sp = 1; sp < row - i - 1; sp++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                if (i == row - 1 && k == i) {
+                    continue;
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 0; j < row - i; j++) {
+                System.out.print("*");
+            }
+            for (int sp = 1; sp <= i; sp++) {
+                System.out.print(" ");
+            }
+            for (int sp = 1; sp < i; sp++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= row - i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < row; i++) {
+            for (int sp = 1; sp <= i; sp++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < row - i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 1; k < row - i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
